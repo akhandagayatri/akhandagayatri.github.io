4ca51ee6aae993f2111896587ba42dcbae7868fa
joaoppneves <joaodasneves@gmail.com>
2017-10-03 00:12:53 -0300

    ahh

--- assets/js/jquery-func.js
+++ assets/js/jquery-func.js
@@ -1,20 +1,35 @@
-! function ($) {
-	//=================================== scroll  ===================================//
+  ! function ($) {
+  	//=================================== scroll  ===================================//
 
-$body.scrollspy({
-      target: '#navbar-main',
-      offset: navHeight
-    })
+  $body.scrollspy({
+        target: '#navbar-main',
+        offset: navHeight
+      })
 
-    $window.on('load', function () {
-      $body.scrollspy('refresh')
-    })
+      $window.on('load', function () {
+        $body.scrollspy('refresh')
+      })
 
-    $('#navbar-main [href=#]').click(function (e) {
-      e.preventDefault()
-    })
+      $('#navbar-main [href=#]').click(function (e) {
+        e.preventDefault()
+      })
 
 
-};
+  };
+
+
+
+  // -------------------------------------------------------------
+      // Animated scrolling / Scroll Up
+      // -------------------------------------------------------------
+
+      (function () {
+        $('li a[href*=#]').bind("click", function(e){
+          var anchor = $(this);
+          $('html, body').stop().animate({ scrollTop: $(anchor.attr('href')).offset().top -50 }, 1000);
+          e.preventDefault();
+        });
+      }());
+
 
 
