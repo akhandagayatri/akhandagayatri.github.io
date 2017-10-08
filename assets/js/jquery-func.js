f4b5e485c22ab0e28ca52a9bef07c47a9621e47e
joaoppneves <joaodasneves@gmail.com>
2017-10-07 23:07:52 -0300

    v

--- assets/js/jquery-func.js
+++ assets/js/jquery-func.js
@@ -1,35 +1,48 @@
-  ! function ($) {
-  	//=================================== scroll  ===================================//
+! function ($) {
+	//=================================== scroll  ===================================//
 
-  $body.scrollspy({
-        target: '#navbar-main',
-        offset: navHeight
-      })
+$body.scrollspy({
+      target: '#navbar-main',
+      offset: navHeight
+    })
 
-      $window.on('load', function () {
-        $body.scrollspy('refresh')
-      })
+    $window.on('load', function () {
+      $body.scrollspy('refresh')
+    })
 
-      $('#navbar-main [href=#]').click(function (e) {
-        e.preventDefault()
-      })
-
-
-  };
+    $('#navbar-main [href=#]').click(function (e) {
+      e.preventDefault()
+    })
 
 
-
-  // -------------------------------------------------------------
-      // Animated scrolling / Scroll Up
-      // -------------------------------------------------------------
-
-      (function () {
-        $('li a[href*=#]').bind("click", function(e){
-          var anchor = $(this);
-          $('html, body').stop().animate({ scrollTop: $(anchor.attr('href')).offset().top -50 }, 1000);
-          e.preventDefault();
-        });
-      }());
+};
 
 
 
+// -------------------------------------------------------------
+    // Animated scrolling / Scroll Up
+    // -------------------------------------------------------------
+
+    (function () {
+      $('li a[href*=#]').bind("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+          scrollTop: $(anchor.attr('href')).offset().top -79}, 1000);
+        e.preventDefault();
+      });
+    }());
+
+
+// -------------------------------------------------------------
+    // Back To Top
+    // -------------------------------------------------------------
+
+    (function () {
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+          $('.scroll-up').fadeIn();
+        } else {
+          $('.scroll-up').fadeOut();
+        }
+      });
+    }());
